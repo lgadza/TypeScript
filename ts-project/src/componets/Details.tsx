dfb1293ea85ccde9c73370dc3bd5e1cf71f01519
Louis Gadza <siuolgadza@gmail.com>
2022-12-08 20:01:08 +0100

    details not working

--- ts-project/src/componets/Details.tsx
+++ ts-project/src/componets/Details.tsx
@@ -1,17 +1,18 @@
 import { Container, Col, Row, Card, Button } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { Blog } from "./types";
-// import BlogDisplay from "./componets/Blog";
+import { format } from "date-fns";
+
 import { useNavigate, useParams } from "react-router-dom";
 
 const Details = () => {
   const [blogDetails, setBlogDetails] = useState<Blog[]>([]);
   const params = useParams();
-  const id = useParams();
   const navigate = useNavigate();
 
-  console.log(blogDetails);
-  console.log(params);
+  // console.log(blogDetails[0]);
+  // console.log(blogDetails.map.);
+  console.log(params.blogId);
   useEffect(() => {
     fetchBlogs();
   }, []);
@@ -19,7 +20,7 @@ const Details = () => {
   const fetchBlogs = async () => {
     try {
       let response = await fetch(
-        `https://api.spaceflightnewsapi.net/v3/articles/${id}`
+        `https://api.spaceflightnewsapi.net/v3/articles/${params.blogId}`
       );
       console.log("RESPONSE", response);
       if (response.ok) {
@@ -35,28 +36,30 @@ const Details = () => {
   };
   return (
     <Container>
-      {/* <Card
-      
-      >
-        <Card.Img variant="top" src={blogDetails.imageUrl} />
-        <Card.Body>
-          <Card.Title>{blogDetails.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted my-4">
-            {blogDetails.newsSite}
-          </Card.Subtitle>
-          <div className="d-flex justify-content-between styleme">
-            <Card.Subtitle className="mb-2 text-muted my-4">
-              Updated date: <br />
-              {format(new Date(blogDetails.updatedAt), "MM/dd/yyyy")}
-            </Card.Subtitle>
-            <Card.Subtitle className="mb-2 text-muted my-4">
-              Publish date: <br />
-              {format(new Date(blogDetails.publishedAt), "MM/dd/yyyy")}
-            </Card.Subtitle>
-          </div>
-          <Card.Text>{blogDetails.summary}</Card.Text>
-          <Card.Link href={blogDetails.url}>Read More</Card.Link>
-        </Card.Body>
+      {/* <Card>
+        {blogDetails.map((detail) => (
+          <>
+            <Card.Img variant="top" src={detail.imageUrl} />
+            <Card.Body>
+              <Card.Title>{detail.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted my-4">
+                {detail.newsSite}
+              </Card.Subtitle>
+              <div className="d-flex justify-content-between styleme">
+                <Card.Subtitle className="mb-2 text-muted my-4">
+                  Updated date: <br />
+                  {format(new Date(detail.updatedAt), "MM/dd/yyyy")}
+                </Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted my-4">
+                  Publish date: <br />
+                  {format(new Date(detail.publishedAt), "MM/dd/yyyy")}
+                </Card.Subtitle>
+              </div>
+              <Card.Text>{detail.summary}</Card.Text>
+              <Card.Link href={detail.url}>Read More</Card.Link>
+            </Card.Body>
+          </>
+        ))}
       </Card> */}
       <div>
         <Button variant="primary" onClick={() => navigate("/")}>
